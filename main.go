e0c09feba145d030dec9ea55960cdb66b5e69697
pedrolima <pedrolima@biofy.tech>
2024-07-29 17:13:30 -0300

    Alterando para criar lista de downTime

--- main.go
+++ main.go
@@ -12,26 +12,30 @@ type Server struct {
 	Server        string
 	ServerUrl     string
 	TempoExecucao float64
+	Status        int
+	DataFalha     string
 }
 
-func main() {
-
-	f, err := os.Open(os.Args[1])
-	if err != nil {
-		fmt.Println(err.Error())
-		panic(err)
-	}
-	defer f.Close()
-
-	csvReader := csv.NewReader(f)
-	records, err := csvReader.ReadAll()
+func openFiles(serversFile string, downtimeFile string) (*os.File, *os.File) {
+	servers, err := os.Open(serversFile)
 	if err != nil {
 		fmt.Println(err.Error())
 		panic(err)
 	}
 
+	downtime, err := os.Open(downtimeFile)
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(err)
+	}
+
+	return servers, downtime
+
+}
+
+func criaListaDeServidores(data [][]string) []Server {
 	var servers []Server
-	for c, record := range records {
+	for c, record := range data {
 		if c > 0 {
 			server := Server{
 				Server:    record[0],
@@ -40,22 +44,47 @@ func main() {
 			servers = append(servers, server)
 		}
 	}
+	return servers
+}
+
+func checkServers(servers []Server) {
+	for _, server := range servers {
+		now := time.Now()
+
+		get, err := http.Get((server.ServerUrl))
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		server.TempoExecucao = time.Since(now).Seconds()
+		server.Status = get.StatusCode
+
+		fmt.Printf("%s Status: [%d] Tempo decorrido: [%f] segundos\n", server.Server, server.Status, server.TempoExecucao)
+	}
+	fmt.Println("--------------------------------------------")
+}
+
+func main() {
+
+	serversList, downtimeList := openFiles(os.Args[1], os.Args[2])
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(serversList)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(err)
+	}
+
+	servidores := criaListaDeServidores(records)
 
 	for {
-		for _, server := range servers {
-			now := time.Now()
-
-			get, err := http.Get(server.ServerUrl)
-			if err != nil {
-				fmt.Println(err.Error())
-				panic(err)
-			}
-			decorrido := time.Since(now).Seconds()
-			status := get.Status
-			fmt.Printf("%s Status: %s Tempo decorrido: %f segundos\n", server.Server, status, decorrido)
-		}
-
-		fmt.Println("--------------------------------------------")
+		checkServers(servidores)
 		time.Sleep(5 * time.Second)
 	}
 }
